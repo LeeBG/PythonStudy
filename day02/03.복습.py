1efc90c15da8bb43a4d668e8cbf4b1a2f5fd9e42
LeeBG <donny1848@gmail.com>
2022-11-08 21:40:50 +0900

    2일차 수업끝(입출력)

---
+++ day02/03.복습.py
@@ -0,0 +1,32 @@
+"""
+＊프로그램 : 목적을 달성하기 위한 절차
+자료 : 변수 + 상수가 보조
+    - 외부에서 받아야 하기 때문에 입력이 동반된다.
+처리 : 연산자 외 기타 등등
+정보 : print를 통해서 출력
+
+변수 : 값(재료)을 위한 저장공간
+상수 : 미리 준비하는 값(재료)
+
+담을 것이 있어야지만 컵이 생긴다.
+
+"""
+name = "홍길동" #문자
+age = 23 # 정수
+height = 176.5 # 실수
+Duty = True #논리값
+
+print(name,age,height,Duty)
+print("이름 : ",name)
+print("나이 :",age)
+print("키 : ",height)
+print("군필 : ",Duty)
+
+# 결과창 글자 크게 하기
+# -파일 ->  기본 설정 -> 설정 -> terminal font ->
+# terminal:integrated:font-size의 숫자를 조절하면 됨
+
+# 언어가 영어로 나온다 !!
+# F1키 누르고, language로 검색
+# configure Display Language를 클릭해서 한국어로 변경
+
---
+++ day02/04.입력EX3.py
@@ -0,0 +1,10 @@
+# input은 마지막에 추가하면 된다.
+print("--입력구간--")
+data1 = input("값1 입력 >>")
+data2 = input("값2 입력 >>")
+# 마지막에 준비하는 것으로 신경써보자
+# input은 값을 대체한다.
+print("--출력구간--")
+print(data1,"와",data2,"입니다.")
+print(data1,",",data2,"은 값입니다.")
+print(data1,"vs",data2,":",data1,"&",data2)
