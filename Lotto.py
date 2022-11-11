314994d812615ba55a4e0348ce45f60e063754ce
LeeBG <donny1848@gmail.com>
2022-11-11 21:51:35 +0900

    5일차 수업끝(서식,이스케이프문자)

--- Lotto.py
+++ Lotto.py
@@ -2,9 +2,10 @@
 import random
 import time
 # 로또만들기
+startTime = time.time() # 실행시간 측정하기
+
 a = list(range(1,46)) #중복체크를 위한 리스트
 b = list()
-startTime = time.time()
 while(len(b)<7):
     num = random.randrange(1,46) # 랜덤
     if num not in a:#중복체크
@@ -15,5 +16,4 @@ while(len(b)<7):
 b.sort()
 print(b)
 endTime = time.time()
-
 print("실행시간은",endTime-startTime,"입니다.")
---
+++ day05/06.문자열EX3.py
@@ -0,0 +1,10 @@
+"""
+조건
+"QUICK" Brown "FOX"
+\JUMP\ 'over'
+"the \LAZY\ DOG"
+한줄로 출력하기
+"""
+# 팁1 : 나중에 붙인다.
+# 팁2 : 한 줄로 만드는 것은 나중에 할 일이다.
+print("\"QUICK\" Brown \"FOX\"\n\\JUMP\\ \'over\'\n\"the \\LAZY\\ DOG\"")
